5cc695ce9c39d46dbcf896fba4807031485dc7ac
ryannb21 <ryanbongwa21@gmail.com>
2025-08-04 20:25:46 -0400

    chore: updated app codes

--- app/ryan_cafe_app/app.py
+++ app/ryan_cafe_app/app.py
@@ -34,7 +34,7 @@ def get_secret(secret_name, region_name="us-east-1"):
 app = Flask(__name__)
 
 #Loading secret key from AWS Secrets Manager
-secret_key = get_secret(os.getenv("FLASK_CAFE_SECRET_NAME", "ryan-cafev7/flask_secret"), region_name=os.getenv("AWS_REGION", "us-east-1"))
+secret_key = get_secret(os.getenv("FLASK_CAFE_SECRET_NAME", "ryan-cafev6/flask_secret"), region_name=os.getenv("AWS_REGION", "us-east-1"))
 if secret_key is None:
     raise ValueError("Failed to retrieve Flask secret key from Secrets Manager")
 app.secret_key = secret_key['secret_key']
@@ -43,7 +43,7 @@ app.secret_key = secret_key['secret_key']
 csrf = CSRFProtect(app)
 
 #Loading email creds securely from AWS Secrets Manager
-email_secrets = get_secret(os.getenv("EMAIL_SECRET_NAME", "ryan-cafev7/emailcreds"), region_name=os.getenv("AWS_REGION", "us-east-1"))
+email_secrets = get_secret(os.getenv("EMAIL_SECRET_NAME", "ryan-cafev6/emailcreds"), region_name=os.getenv("AWS_REGION", "us-east-1"))
 if email_secrets is None:
     raise ValueError("Failed to retrieve email secrets from Secrets Manager")
 #Gmail SMTP configuration
@@ -58,7 +58,7 @@ mail = Mail(app)
 
 
 #Loading db creds securely from AWS Secrets Manager
-db_secrets = get_secret(os.getenv("DB_SECRET_NAME", "ryan-cafev7/db_creds"), region_name=os.getenv("AWS_REGION", "us-east-1"))
+db_secrets = get_secret(os.getenv("DB_SECRET_NAME", "ryan-cafev6/db_creds"), region_name=os.getenv("AWS_REGION", "us-east-1"))
 if db_secrets is None:
     raise ValueError("Failed to retrieve database secrets from Secrets Manager")
 #DB CONFIG
