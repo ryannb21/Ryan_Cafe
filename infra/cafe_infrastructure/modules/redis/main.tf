44797018e4169489414c315b70f2ca119a0b5d3f
Ryan B <ryanbongwa21@gmail.com>
2026-02-23 23:05:08 -0500

    Perfomed upgrade from Redis 6.2 to Valkey 7.2 with apply_immediately condition set to 'true'

--- infra/cafe_infrastructure/modules/redis/main.tf
+++ infra/cafe_infrastructure/modules/redis/main.tf
@@ -5,15 +5,15 @@ resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   tags       = var.common_tags
 }
 
-# ElastiCache Replication Group (Redis 6.2 - temporary revert)
+# ElastiCache Replication Group (Valkey 7.2)
 resource "aws_elasticache_replication_group" "redis" {
   replication_group_id       = var.cluster_id
   description                = "Valkey replication group"
-  engine                     = "redis"
-  engine_version             = "6.2"
+  engine                     = "valkey"
+  engine_version             = "7.2"
   node_type                  = var.node_type
   num_cache_clusters         = 1
-  parameter_group_name       = "default.redis6.x"
+  parameter_group_name       = "default.valkey7"
   port                       = 6379
   subnet_group_name          = aws_elasticache_subnet_group.redis_subnet_group.name
   security_group_ids         = [var.security_group_id]
